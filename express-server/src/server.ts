dd9b4d7635b529b8b2e4f65d97f5a128f3b64429
Alex <devking0718@gmail.com>
2024-08-23 20:12:35 +0900

    update backend

--- express-server/src/server.ts
+++ express-server/src/server.ts
@@ -10,58 +10,39 @@ const generateFakeTransactionHash = (): string => {
   ).join('');
 }
 
-const generateFakeMethod = (): string => {
-  const methods = ['mint', 'burn', 'swap'];
-  return methods[Math.floor(Math.random() * methods.length)];
-}
-
 const generateFakeAddress = (): string => {
   return '0x' + Array.from({ length: 40 }, () =>
     Math.floor(Math.random() * 16).toString(16)
   ).join('');
 }
 
-const generateFakeDecoded = (): any => {
-  const isSwapIn0 = Math.random() < 0.5;
-  const amountIn = Math.floor(Math.random() * 1e9 + 1e6).toString();
-  const amountOut = Math.floor(Math.random() * 1e9 + 1e6).toString();
-  return {
-    to: generateFakeAddress(),
-    sender: generateFakeAddress(),
-    amount0In: isSwapIn0 ? amountIn : "0",
-    amount1In: isSwapIn0 ? "0" : amountIn,
-    amount0Out: isSwapIn0 ? "0" : amountOut,
-    amount1Out: isSwapIn0 ? amountOut : "0",
-  };
-}
-
 const pool = new Pool({
   connectionString: process.env.PG_DATABASE_URL
 });
 
-let lastBlockNumber = Math.floor(Math.random() * 1000000);
 
-const generateNextBlockNumber = (): number => {
-  const increment = Math.floor(Math.random() * 10) + 1; // Random increment between 1 and 10
-  lastBlockNumber += increment;
-  return lastBlockNumber;
+const generateFakeActivities = (): string => {
+  const isBridged = Math.random() < 0.5;
+  return isBridged ? "Bridged" : "Transaction"
+}
+
+const generateFakePoints = (): number => {
+    const points = Math.floor(Math.random() * 1000) + 1; // Random increment between 1 and 10
+    return points;
 }
 
 const insertRandomLog = async () => {
   const client = await pool.connect();
   try {
     const query = `
-      INSERT INTO logs (transaction_hash, decoded, address, block_number, block_timestamp, "from", "to")
-      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      INSERT INTO logs (activity, points, block_timestamp, transaction_hash)
+      VALUES ($1, $2, $3, $4)
     `;
     const values = [
-      generateFakeTransactionHash(),
-      JSON.stringify(generateFakeDecoded()),
-      generateFakeAddress(),
-      generateNextBlockNumber(),
+      generateFakeActivities(),
+      generateFakePoints(),
       new Date().toISOString(),
-      generateFakeAddress(),
-      generateFakeAddress()
+      generateFakeTransactionHash(),
     ];
 
     await client.query(query, values);
